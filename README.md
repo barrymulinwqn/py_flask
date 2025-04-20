8fb0b2e6f0ffe623c1c32e40b245abdf9061055a
barrymulinwqn <xueegdlut@163.com>
2025-04-20 21:23:49 +0800

    init sqlite db

--- README.md
+++ README.md
@@ -16,3 +16,21 @@
 ## 部署前的准备
 首先，我们需要生成一个依赖列表，方便在部署环境里安装。使用下面的命令把当前依赖列表写到一个 requirements.txt 文件里：
 > (env) $ pip freeze > requirements.txt
+
+
+## 创建数据库表
+- 模型类创建后，还不能对数据库进行操作，因为我们还没有创建表和数据库文件。下面在 Python Shell 中创建了它们
+  > (env) $ flask shell
+  >
+    > from app import db
+    > 
+    >  db.create_all()
+    
+- 打开文件管理器，你会发现项目根目录下出现了新创建的数据库文件 data.db。这个文件不需要提交到 Git 仓库，我们在 .gitignore 文件最后添加一行新规则：  
+    > *.db
+- 如果你改动了模型类，想重新生成表模式，那么需要先使用 db.drop_all() 删除表，然后重新创建：
+    > db.drop_all()
+    > 
+    > db.create_all()
+    > 
+- 注意这会一并删除所有数据，如果你想在不破坏数据库内的数据的前提下变更表的结构，需要使用数据库迁移工具，比如集成了 Alembic 的 Flask-Migrate 扩展。
--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+import os
+import sys
+
+import click
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 app.config.update(DEBUG=True)
+
+
 
 name = 'Grey Li'
 movies = [
@@ -27,3 +44,39 @@ def index():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+##  ORM
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+def addnewdatatodb():
+    """Add new data to SQLite db database."""
+    from app import User, Movie
+    user = User(name='Grey Li')
+    m1 = Movie(title='Leon', year='1994')
+    m2 = Movie(title='Mahjong', year='1996')
+    db.session.add(user)
+    db.session.add(m1)
+    db.session.add(m2)
+    db.session.commit()
+    click.echo('Add new User and Movies data to SQLite db database successfully.')  # 输出提示信息
