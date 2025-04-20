302d967e28eb3d23683c2ad47d3c2e69a828c0c6
barrymulinwqn <xueegdlut@163.com>
2025-04-20 10:53:44 +0800

    ini version

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.update(DEBUG=True)
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
